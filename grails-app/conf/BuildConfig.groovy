11f14a74ddb2868396680dc2ae1fd6ac1da67850
Bob Florian <bflorian@gmail.com>
2012-04-20 17:58:18 -0400

    Plugin repository changes.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -46,3 +46,11 @@ grails.project.dependency.resolution = {
 		// runtime 'mysql:mysql-connector-java:5.1.13'
     }
 }
+grails.project.repos.beanstalkRepository.url = "https://mural.svn.beanstalkapp.com/grails-plugins"
+grails.project.repos.beanstalkRepository.type = "svn"
+
+grails.project.repos.dreamhostRepository.url = "http://cm.florian.org/grailsplugins"
+grails.project.repos.dreamhostRepository.type = "svn"
+
+grails.project.repos.default = "dreamhostRepository"
+grails.release.scm.enabled = false
