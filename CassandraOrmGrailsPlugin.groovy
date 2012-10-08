718a6326d9977844fd8b14a84cc990347baa37de
Bob Florian <bflorian@gmail.com>
2012-10-08 12:47:19 -0400

    Version 0.2.5

--- CassandraOrmGrailsPlugin.groovy
+++ CassandraOrmGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import com.reachlocal.grails.plugins.cassandra.uuid.UuidDynamicMethods
 class CassandraOrmGrailsPlugin
 {
 	// the plugin version
-	def version = "0.2.4"
+	def version = "0.2.5"
 
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "2.0.0 > *"
