bc51fa05d1ca186b77c979f4cbce22eab90067ba
Bob Florian <bflorian@gmail.com>
2014-01-29 13:43:47 -0600

    1.0.5 release.

--- CassandraOrmGrailsPlugin.groovy
+++ CassandraOrmGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import com.reachlocal.grails.plugins.cassandra.uuid.UuidDynamicMethods
 
 class CassandraOrmGrailsPlugin
 {
-	def version = "1.0.5-SNAPSHOT"
+	def version = "1.0.5"
 	def grailsVersion = "2.0.0 > *"
 	def author = "Bob Florian"
 	def authorEmail = "bob.florian@reachlocal.com"
